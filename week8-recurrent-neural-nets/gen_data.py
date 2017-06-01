d4e0d2de5a74c14f4a12ea27cfdacb1fbfd7212c
Rohan Varma <rvarm1@gmail.com>
2017-06-01 11:33:55 -0700

    being stupid

---
+++ week8-recurrent-neural-nets/gen_data.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def gen_data(size=1000000):
+    X = np.array(np.random.choice(2, size=(size,)))
+    Y = []
+    for i in range(size):
+        threshold = 0.5
+        if X[i-3] == 1:
+            threshold += 0.5
+        if X[i-8] == 1:
+            threshold -= 0.25
+        if np.random.rand() > threshold:
+            Y.append(0)
+        else:
+            Y.append(1)
+    return X, np.array(Y)
